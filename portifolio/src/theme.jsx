d33656f9e76c418ff27e68bb71bb82142d1368f3
Mariana Souza <marsouzafranca@gmil.com>
2025-11-07 01:21:46 -0300

    feat: Criando o theme da pagina

--- portifolio/src/theme.jsx
+++ portifolio/src/theme.jsx
@@ -3,10 +3,17 @@ import { createTheme } from '@mui/material/styles';
 let theme = createTheme({
   palette: {
     primary: {
-      main: '#1D1616',
+      main: '#171717',
     },
     secondary: {
       main: '#8E1616',
+      light: '#a60000',
+    },
+    tertiary: {
+      main: '#000000ff', 
+    },
+    quaternary: {
+      main: '#F9F8F6',
     },
   },
   typography: {
