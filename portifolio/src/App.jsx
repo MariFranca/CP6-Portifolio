eb9e3eef38b2ca684b1a76e79432d462391cbbf8
Mariana Souza <marsouzafranca@gmil.com>
2025-11-07 19:00:46 -0300

    feat: Adicionando as rotas

--- portifolio/src/App.jsx
+++ portifolio/src/App.jsx
@@ -1,12 +1,29 @@
-import Home from "./pages/Home/Home"
+import NavBar from "./components/NavBar/NavBar";
+import Home from "./pages/Home/Home";
+import About from "./pages/Home/sections/About/About";
+import Projects from "./pages/Home/sections/Projects/Projects";
+import Contact from "./pages/Home/Contact/Contact";
+import Footer from "./pages/Home/Footer/Footer";
 
-const App = () =>  {
-
+const App = () => {
   return (
     <>
-      <Home />
+      <NavBar />
+      <section id="home">
+        <Home />
+      </section>
+      <section id="about">
+        <About />
+      </section>
+      <section id="projects">
+        <Projects />
+      </section>
+      <section id="contact">
+        <Contact />
+      </section>
+      <Footer />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- portifolio/src/components/NavBar/NavBar.jsx
+++ portifolio/src/components/NavBar/NavBar.jsx
@@ -1,26 +1,49 @@
-import {AppBar, MenuItem, Toolbar, styled } from "@mui/material";
+import { AppBar, MenuItem, Toolbar, styled } from "@mui/material";
+import { Link } from "react-scroll";
 
 const StyledToolbar = styled(Toolbar)(() => ({
-    display: "flex",
-    justifyContent: "end",
-    gap: "60px",
-    
+  display: "flex",
+  justifyContent: "end",
+  gap: "60px",
 }));
 
-const NavBar = () =>  {
+const StyledLink = styled(Link)(() => ({
+  textDecoration: "none",
+  color: "#171717",
+  fontWeight: "500",
+  cursor: "pointer",
+  "&:hover": {
+    color: "tertiary.main", 
+  },
+}));
 
+const NavBar = () => {
   return (
-    <>
-      <AppBar position="absolute" color="secondary" >
-            <StyledToolbar>
-                <MenuItem >About me</MenuItem>
-                <MenuItem>Skills</MenuItem>
-                <MenuItem>Projects</MenuItem>
-            </StyledToolbar>
-      </AppBar>
-    </>
-  )
-}
+    <AppBar position="absolute" color="secondary">
+      <StyledToolbar>
+        <MenuItem>
+          <StyledLink to="home" smooth={true} duration={600} offset={-70}>
+            Home
+          </StyledLink>
+        </MenuItem>
+        <MenuItem>
+          <StyledLink to="about" smooth={true} duration={600} offset={-70}>
+            About me
+          </StyledLink>
+        </MenuItem>
+        <MenuItem>
+          <StyledLink to="projects" smooth={true} duration={600} offset={-70}>
+            Projects
+          </StyledLink>
+        </MenuItem>
+        <MenuItem>
+          <StyledLink to="contact" smooth={true} duration={600} offset={-70}>
+            Contact
+          </StyledLink>
+        </MenuItem>
+      </StyledToolbar>
+    </AppBar>
+  );
+};
 
-export default NavBar
-
+export default NavBar;
